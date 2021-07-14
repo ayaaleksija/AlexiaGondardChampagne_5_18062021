1a38e313a9c0fec38c153e8190f6488d813f6441
Alexia Gondard-Champagne <a.gondardchamp@gmail.com>
2021-07-14 09:18:14 +0200

    création de la class objet de mon produit + création de la fonction de formatage et d'affichage et du prix en euros

--- front-end/JavaScript/fonctions.js
+++ front-end/JavaScript/fonctions.js
@@ -2,3 +2,23 @@
 
 const url = "http://localhost:3000/api/teddies";
 const urlId = "http://localhost:3000/api/teddies/${idProduit}";
+
+// prix en cts, fonction de conversion du prix en euros
+function conversionPrix(produitPrix) {
+    let prix = `${produitPrix}`;
+    prix = Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR', code: "€", minimumFractionDigits: 2, }).format(prix / 100);
+    return prix;
+}
+
+// création de l'objet / class de mon nounours
+class Produit {
+    constructor(id, nom, description, prix, couleur, quantite, image) {
+        this.id = id;
+        this.name = nom;
+        this.description = description;
+        this.prix = prix;
+        this.couleur = couleur;
+        this.quantite = quantite;
+        this.image = image;
+    }
+}
