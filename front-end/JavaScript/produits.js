2b01dcedddd3396f3670e3b3d1283057ba9652a8
Alexia Gondard-Champagne <a.gondardchamp@gmail.com>
2021-07-14 23:13:50 +0200

    ajout de l'option de couleur

--- front-end/JavaScript/produits.js
+++ front-end/JavaScript/produits.js
@@ -5,11 +5,13 @@ const urlID = params.get('_id');
 // déclaration de ma nouvelle id dans mes fonctions - paramètres d'environnement
 const idProduit = "http://localhost:3000/api/teddies/" + urlID;
 
+
 // requête avec la nouvelle adresse 
 fetch(idProduit)
     .then((produit) => produit.json())
     .then((produit) => {
         getFicheProduit(produit);
+        choixCouleur(produit);
     })
     // gestion des erreurs
     .catch(function(error) {
@@ -18,15 +20,22 @@ fetch(idProduit)
 
 // fonction de création de la fiche produit 
 function getFicheProduit(produit) {
-    let ficheProduit = document.getElementById("produits")
-        // déclaration de la variable prix pour utiliser cette variable formatée
-    let prix = conversionPrix(produit.price);
-    ficheProduit.innerHTML +=
-        `<div class="col-sm-12 col-md-6 pb-3  ">
-            <div class="card bg-light md-5 border rounded p-3">
-                <img src="${produit.imageUrl}" class="img-fluid p-1 img-thumbnail" alt="${produit.name}">
-                <h3 class="card-title">${produit.name}</h3>
-                <h4 class="card-title">${prix}</h4>
-            <div>
-        </div>`;
+    let produitImage = document.getElementById("imageProduit");
+    produitImage.innerHTML += `<img src="${produit.imageUrl}" class="img-fluid img-thumbnail" alt="${produit.name}">`;
+    let produitNom = document.getElementById("nomProduit");
+    produitNom.innerHTML += `<h3>${produit.name}</h3>`;
+    let produitDescription = document.getElementById("descriptionProduit");
+    produitDescription.innerHTML += `<h3>${produit.description}</h3>`;
+    let produitPrix = document.getElementById("prixProduit");
+    produitPrix.innerHTML += `<h4>${conversionPrix(produit.price)}</h4>`;
+}
+
+// fonction pour le choix de la couleur
+function choixCouleur(produit) {
+    let choixCouleur = document.getElementById("choixCouleur");
+    for (let i = 0; i < produit.colors.length; i++) {
+        let option = document.createElement("option");
+        option.innerText = produit.colors[i];
+        choixCouleur.appendChild(option);
+    }
 }
