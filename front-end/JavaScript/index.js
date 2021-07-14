394a3fad2141b555320231456e3ffab55d0b8e92
Alexia Gondard-Champagne <a.gondardchamp@gmail.com>
2021-07-14 09:19:00 +0200

    utilisation de la fonction de formatage de prix

--- front-end/JavaScript/index.js
+++ front-end/JavaScript/index.js
@@ -15,14 +15,15 @@ function addFiche(data) {
     for (produit of data) {
         //recupère l'élément "liste" dans le HTML
         let fiche = document.getElementById("liste");
+        // déclaration de la variable prix pour utiliser cette variable formatée
+        let prix = conversionPrix(produit.price);
         // création de la fiche produit
         fiche.innerHTML +=
             `<div class="col-sm-12 col-md-6 pb-3  ">
                 <div class="card bg-light md-5 border rounded p-3">
                         <img src="${produit.imageUrl}" class="img-fluid p-1 img-thumbnail" alt="${produit.name}">
                         <h3 class="card-title">${produit.name}</h3>
-                        <h4 class="card-title">${produit.price}</h4>
-                        <p class="card-text">${produit.description}</p>
+                        <h4 class="card-title">${prix}</h4>
                         <a href="./front-end/produits.html?_id=${produit._id}"><button> Voir plus </button></a>
                 <div>
             </div>`;
