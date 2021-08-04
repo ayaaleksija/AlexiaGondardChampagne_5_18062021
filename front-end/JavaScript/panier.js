15acb801de8ccfa16b07aee98b2ea2cf74fd0c12
Alexia Gondard-Champagne <a.gondardchamp@gmail.com>
2021-08-04 12:48:53 +0200

    création de la requete post avec envoi des infos.. mais ca marche pas bien evidemment

--- front-end/JavaScript/panier.js
+++ front-end/JavaScript/panier.js
@@ -1,20 +1,24 @@
+////// utilisation de JSON.PARSE pour récupérer les données présentes dans localstorage
+let localS = JSON.parse(localStorage.getItem("produit"));
+
 main();
 
 function main() {
     afficherPanier();
     viderPanier();
     afficherFormulaire();
+    validationFormulaireCommande();
 }
 
+
+
 function afficherPanier() {
-    ////// utilisation de JSON.PARSE pour récupérer les données présentes dans localstorage
-    let localS = JSON.parse(localStorage.getItem("produit"));
     ////// récupération de mes variables
     const panierPlein = document.getElementById("panierPlein");
     const formulaireCommande = document.getElementById("formulaireCommande");
     const panierVide = document.getElementById("panierVide");
     ////// si le panier est vide
-    if (localS < 1) {
+    if (localStorage.getItem("produit") < 1) {
         panierPlein.classList.add("d-none");
         formulaireCommande.classList.add("d-none");
     } else {
@@ -56,5 +60,57 @@ function afficherFormulaire() {
     btnValidationPanier.addEventListener("click", (e) => {
         e.preventDefault;
         formulaireCommande.classList.toggle("d-none");
+    });
+}
+
+
+function validationFormulaireCommande() {
+    const regexName = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+))$/;
+    const regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$/;
+    const regexAddress = /^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/;
+    const regexCity = /^(([a-zA-ZÀ-ÿ]+[\s\-]{1}[a-zA-ZÀ-ÿ]+)|([a-zA-ZÀ-ÿ]+)){1,10}$/;
+    // création de l'objet fiche client
+    let ficheClient = {
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        address: document.getElementById("address").value,
+        city: document.getElementById("city").value,
+        email: document.getElementById("email").value,
+    };
+    console.log(ficheClient);
+
+    const validationCommande = document.getElementById("order");
+    validationCommande.addEventListener("click", (e) => {
+        e.preventDefault();
+        // condition pour valider si le formulaire est correctement rempli par le client
+        if (((regexName.test(ficheClient.firstName) === false) ||
+                (regexEmail.test(ficheClient.email) === false) ||
+                (regexAddress.test(ficheClient.address) === false) ||
+                (regexCity.test(ficheClient.city) === false))) {
+            messageError = "Merci de vérifier les champs à compléter.";
+        }
+        // déclaration d'un tableai des produits acheté à push
+        let produitsAchetes = [];
+        produitsAchetes.push(localS);
+
+        let commande = {
+            contact: ficheClient,
+            products: produitsAchetes,
+        };
+
+        // Envoi de la requete POST pour le backend
+        const choixClient = {
+            method: "POST",
+            body: JSON.stringify(commande),
+            headers: { "Content-Type": "application/json" },
+        };
+
+        fetch("http://localhost:3000/api/teddies/order", choixClient)
+            .then((response) => response.json())
+            .then((data) => {
+                localStorage.setItem("commande", JSON.stringify(data));
+                document.location.href = "commande.html";
+            })
+            .catch((erreur) => console.log("erreur : " + erreur));
     });
 }
